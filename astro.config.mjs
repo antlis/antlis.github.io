be603c412da2ef10047ae4c5214eacafefca625f
lad <antlis@protonmail.com>
2026-05-13 10:25:52 +0500

    Replace gists with astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    expressiveCode({
+      themes: ['github-dark', 'github-light'],
+      themeCssSelector: (theme) => {
+        const isDark = theme.type === 'dark'
+        return `[data-theme='${isDark ? 'dark' : 'light'}']`
+      },
+    }),
+    mdx(),
+  ],
   output: 'static',
   i18n: {
     defaultLocale: 'en',
